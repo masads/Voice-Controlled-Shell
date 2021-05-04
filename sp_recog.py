eaa5493a99cd23171300895825cbb247503d99cc
masads <asadsummair2019@gmail.com>
2021-05-04 13:58:59 -0700

    70%

--- sp_recog.py
+++ sp_recog.py
@@ -2,10 +2,11 @@ import speech_recognition as sr
 import rcmd
 
 def main():
-
+	
     r=sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
+        person_speech = ""
 
         print("Say Something..")
 
@@ -14,13 +15,14 @@ def main():
         print("Recongnizing Now...")
 
         try:
-            person_speech = r.recognize_google(audio, language="hi-IN")
-            print("you have said: \n"+ person_speech)
+            person_speech = r.recognize(audio)
+            print("Speech was:" + person_speech)
 
         except Exception as e:
             print("Error: "+ str(e))    
-        
-	#rcmd.run_cmd_py("ls")	
+        if "current working directory" in person_speech or "where I am standing" in person_speech:
+		rcmd.run_cmd_py("pwd")
+		
 
 main()
            
